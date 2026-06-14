1166367f02e0eecf5c2edd9e19286e914d6a94b3
ardririy <hynnkgw27@gmail.com>
2026-06-14 21:34:33 +0900

    configのほうがいいかも+starship

---
+++ config/starship.nix
@@ -0,0 +1,122 @@
+{ config, pkgs, ... }:
+{
+  programs.starship = {
+    enable = true;
+    settings = {
+      add_newline = false;
+      character = {
+        success_symbol = "[λ](bold green)";
+        error_symbol = "[λ](bold red)";
+      };
+      format = builtins.concatStringsSep "" [
+        "$username"
+        "$hostname"
+        # "$localip"
+        "$shlvl"
+        # "$singularity"
+        "$kubernetes"
+        "$directory"
+        # "$vcsh"
+        # "$fossil_branch"
+        # "$fossil_metrics"
+        "$git_branch"
+        "$git_commit"
+        "$git_state"
+        "$git_metrics"
+        "$git_status"
+        # "$hg_branch"
+        # "$pijul_channel"
+        # "$docker_context"
+        "$package"
+        "$c"
+        "$cmake"
+        # "$cobol"
+        # "$daml"
+        # "$dart"
+        # "$deno"
+        # "$dotnet"
+        "$elixir"
+        # "$elm"
+        "$erlang"
+        # "$fennel"
+        "$gleam"
+        "$golang"
+        # "$guix_shell"
+        "$haskell"
+        # "$haxe"
+        "$helm"
+        # "$java"
+        "$julia"
+        # "$kotlin"
+        # "$gradle"
+        # "$lua"
+        # "$nim"
+        # "$nodejs"
+        "$ocaml"
+        # "$opa"
+        "$perl"
+        # "$php"
+        # "$pulumi"
+        # "$purescript"
+        "$python"
+        # "$quarto"
+        # "$raku"
+        # "$rlang"
+        # "$red"
+        # "$ruby"
+        "$rust"
+        # "$scala"
+        # "$solidity"
+        # "$swift"
+        "$terraform"
+        # "$typst"
+        # "$vlang"
+        # "$vagrant"
+        # "$zig"
+        # "$buf"
+        "$nix_shell"
+        # "$conda"
+        # "$meson"
+        # "$spack"
+        "$memory_usage"
+        "$aws"
+        # "$gcloud"
+        # "$openstack"
+        # "$azure"
+        # "$nats"
+        # "$direnv"
+        # "$env_var"
+        # "$crystal"
+        # "$custom"
+        # "$sudo"
+        "$cmd_duration"
+        # "$line_break"
+        "$jobs"
+        # "$battery"
+        "$time"
+        "$line_break" # added
+        "$status"
+        # "$os"
+        # "$container"
+        # "$shell"
+        "$character"
+      ];
+      git_branch.symbol = "🌱 ";
+      git_commit.tag_disabled = false;
+      git_status = {
+        ahead = ''⇡''${count}'';
+        behind = ''⇣''${count}'';
+        diverged = ''⇕⇡''${ahead_count}⇣''${behind_count}'';
+        staged = "+$count";
+      };
+      kubernetes.disabled = false;
+      nix_shell = {
+        format = "via [$symbol$state]($style) ";
+        impure_msg = "ι";
+        pure_msg = "﻿ρ";
+        symbol = "❄️";
+      };
+      time.disabled = false;
+    };
+  };
+}
--- home.nix
+++ home.nix
@@ -2,7 +2,8 @@
 
 {
   imports = [
-    ./settings/zsh.nix
+    ./config/zsh.nix
+    ./config/starship.nix
   ];
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
